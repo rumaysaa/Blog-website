4be3342bff8bd972dad45a220a204124b339c4df
Rumaysa babulkhair <rumaysa.babulkhair@gmail.com>
2026-01-06 17:26:28 +0000

    comments

--- routers/comments.js
+++ routers/comments.js
@@ -6,45 +6,72 @@ const users = require('../modules/users')
 
 
 router.post('/', async (req, res) => {
-    const result = await comments.addComment(req.body)
-    res.redirect('/comments?id=' + req.body.articleID)
+    try {
+        const result = await comments.addComment(req.body)
+        res.redirect('/comments?id=' + req.body.articleID)
+    } catch(err) {
+        console.error('Error adding comment:', err)
+        res.status(500).send('Error adding comment')
+    }
 })
 
 
 router.get('/', async (req, res) => {
-    const id = req.query.id
-    const Article = await article.getArticleById(id)
-    const com = await comments.getArticleById(id)
-    const allComments = await com.map(async data => {
-        data.imageBase64 = await data.userID.profilePhoto.toString('base64')
-        return data
-    })
-    let imageBase64=""
-    if (req.session.user) {
-        if(req.session.user.profilePhoto){
-        const img = await users.findUserById(req.session.user._id)
-        imageBase64 = img.profilePhoto.toString('base64')
+    try {
+        const id = req.query.id
+        const Article = await article.getArticleById(id)
+        const com = await comments.getArticleById(id)
+        
+        const allComments = await Promise.all(com.map(async data => {
+            if (data.userID && data.userID.profilePhoto) {
+                data.imageBase64 = data.userID.profilePhoto.toString('base64')
+            } else {
+                data.imageBase64 = ''
+            }
+            return data
+        }))
+        
+        let imageBase64=""
+        if (req.session.user) {
+            if(req.session.user.profilePhoto){
+                const img = await users.findUserById(req.session.user._id)
+                if(img && img.profilePhoto){
+                    imageBase64 = img.profilePhoto.toString('base64')
+                }
+            }
         }
-    }
 
-    const data = { Article, com, imageBase64 }
-    //console.log(data.com)
-    res.render('readArticle', { data })
+        const data = { Article, com: allComments, imageBase64 }
+        res.render('readArticle', { data })
+    } catch(err) {
+        console.error('Error loading comments:', err)
+        res.status(500).send('Error loading article')
+    }
 })
 
 router.get('/json', async (req, res) => {
-    const id = req.query.id
-    const Article = await article.getArticleById(id)
-    const com = await comments.getArticleById(id)
-    const imageBase64 = ""
-    const data = { Article, com, imageBase64 }
-    //console.log(data);
-    res.send(data)
+    try {
+        const id = req.query.id
+        const Article = await article.getArticleById(id)
+        const com = await comments.getArticleById(id)
+        const imageBase64 = ""
+        const data = { Article, com, imageBase64 }
+        res.send(data)
+    } catch(err) {
+        console.error('Error in /json route:', err)
+        res.status(500).send({ error: 'Error loading data' })
+    }
 })
 
 router.get('/delete' , async (req,res) => {
-    const id = await req.query.id
-    comments.removeCommentById(id).then(res.redirect('/comments?id='+ req.query.articleID))
+    try {
+        const id = await req.query.id
+        await comments.removeCommentById(id)
+        res.redirect('/comments?id='+ req.query.articleID)
+    } catch(err) {
+        console.error('Error deleting comment:', err)
+        res.status(500).send('Error deleting comment')
+    }
 })
 
 module.exports = router
--- routers/users.js
+++ routers/users.js
@@ -35,41 +35,64 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    if (req.body.confirmPass === req.body.password) {
-        const name = await req.body.name
-        const mail = await req.body.mail.toLowerCase()
-        const checkMail = await users.findUserByMail(req.body)
-        if(checkMail){
-            const msg = "Mail Already Exists!"
-            res.redirect('/users?Mailmsg='+msg+'&mail='+mail)
+    try {
+        if (!req.body.name || !req.body.mail || !req.body.password) {
+            return res.redirect('/users?msg=All fields are required')
         }
-        const salt = await bcrypt.genSalt(10)
-        const password = await bcrypt.hash(req.body.password, salt)
-        const user = { name, mail, password }
-        users.createUser(user).then(values => {
-            const send = require('gmail-send')({
-                user: process.env.MAIL_USERNAME,
-                pass: process.env.MAIL_PASSWORD
-            });
-            send({
-                to: req.body.mail,
-                subject: 'Account Activation',
-                text: 'Welcome!!!!!!',
-                html: '<a href="https://theblogosphere.herokuapp.com/users/activate?id=' + values.id + '">click here to Activate Your Account</a>'
-            }, (error, result, fullResult) => {
-                if (error) console.error(error);
+        if (req.body.password.length < 6) {
+            return res.redirect('/users?msg=Password must be at least 6 characters')
+        }
+        if (req.body.confirmPass === req.body.password) {
+            const name = await req.body.name
+            const mail = await req.body.mail.toLowerCase()
+            const checkMail = await users.findUserByMail(req.body)
+            if(checkMail){
+                const msg = "Mail Already Exists!"
+                res.redirect('/users?Mailmsg='+msg+'&mail='+mail)
+                return
+            }
+            const salt = await bcrypt.genSalt(10)
+            const password = await bcrypt.hash(req.body.password, salt)
+            const user = { name, mail, password }
+            users.createUser(user).then(values => {
+                const send = require('gmail-send')({
+                    user: process.env.MAIL_USERNAME,
+                    pass: process.env.MAIL_PASSWORD
+                });
+                const baseUrl = process.env.BASE_URL || `http://${req.get('host')}`
+                const activationLink = `${baseUrl}/users/activate?id=${values.id}`
+                send({
+                    to: req.body.mail,
+                    subject: 'Account Activation',
+                    text: 'Welcome!!!!!!',
+                    html: `<a href="${activationLink}">click here to Activate Your Account</a>`
+                }, (error, result, fullResult) => {
+                    if (error) console.error(error);
+                })
+                const activateMsg = "Kindly Check Your Mail to Activate Your Account."
+                res.redirect('/users/login?activateMsg=' + activateMsg)
+            }).catch(err => {
+                console.error('Error creating user:', err)
+                res.status(500).send('Error creating account')
             })
-            const activateMsg = "Kindly Check Your Mail to Activate Your Account."
-            res.redirect('/users/login?activateMsg=' + activateMsg)
-        })
-    }
-    else {
-        res.redirect('/users?msg=Password Does\'nt Match&name='+req.body.name+'&mail='+req.body.mail)
+        }
+        else {
+            res.redirect('/users?msg=Password Does\'nt Match&name='+req.body.name+'&mail='+req.body.mail)
+        }
+    } catch(err) {
+        console.error('Error in registration:', err)
+        res.status(500).send('Error during registration')
     }
 })
-router.get('/activate', (req, res) => {
-    const msg="Succesfully Activated Your Account. Login to Continue"
-    users.activate(req.query.id).then(value => res.redirect('/users/login?msg='+msg))
+router.get('/activate', async (req, res) => {
+    try {
+        const msg="Succesfully Activated Your Account. Login to Continue"
+        await users.activate(req.query.id)
+        res.redirect('/users/login?msg='+msg)
+    } catch(err) {
+        console.error('Error activating account:', err)
+        res.status(500).send('Error activating account')
+    }
 })
 
 router.get('/login', async (req, res) => {
@@ -86,89 +109,155 @@ router.get('/login', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    const foundUser = await users.findUserByMail(req.body)
+    try {
+        if (!req.body.mail || !req.body.password) {
+            return res.redirect('/users/login?passMsg=Email and password required')
+        }
+        const foundUser = await users.findUserByMail(req.body)
 
-    if (!foundUser) {
-        res.redirect('/users/login?passMsg=Email not registered!');
-    }
-    const comparePass = await (bcrypt.compare(req.body.password, foundUser.password))
-    if (comparePass) {
-        if (foundUser.active) {
-            req.session.user = foundUser
-            if (req.body.url)
-                res.redirect(req.body.url+ "?id=" + req.body.articleID)
-            else
-            //console.log(req.session.user)
-            res.redirect('/articles')
+        if (!foundUser) {
+            res.redirect('/users/login?passMsg=Email not registered!');
+            return
+        }
+        const comparePass = await (bcrypt.compare(req.body.password, foundUser.password))
+        if (comparePass) {
+            if (foundUser.active) {
+                req.session.user = foundUser
+                if (req.body.url)
+                    res.redirect(req.body.url+ "?id=" + req.body.articleID)
+                else
+                    res.redirect('/articles')
+            }
+            else {
+                res.redirect('/users/login?activateMsg=Your Account is not activated.Kindly Check your mail to Activate.')
+            }
         }
         else {
-            res.redirect('/users/login?activateMsg=Your Account is not activated.Kindly Check your mail to Activate.')
+            const passMsg = "Incorrect Password"
+            const mail = req.body.mail
+            res.redirect('/users/login?passMsg=' + passMsg + '&mail=' + mail)
         }
-    }
-    else {
-        const passMsg = "Incorrect Password"
-        const mail = req.body.mail
-        res.redirect('/users/login?passMsg=' + passMsg + '&mail=' + mail)
+    } catch(err) {
+        console.error('Error during login:', err)
+        res.status(500).send('Error during login')
     }
 })
 
 router.get('/logout', (req, res) => {
-    req.session.destroy(function (err) {
-        res.redirect('/articles')
-    })
+    try {
+        req.session.destroy(function (err) {
+            if (err) {
+                console.error('Error destroying session:', err)
+                return res.status(500).send('Error logging out')
+            }
+            res.redirect('/articles')
+        })
+    } catch(err) {
+        console.error('Error during logout:', err)
+        res.status(500).send('Error logging out')
+    }
 })
 
 router.get('/editUser', async (req, res) => {
-    const user = await req.session.user
-    const img = await users.findUserById(user._id)
-    let imageBase64=""
-    if(img.profilePhoto){
-    imageBase64 = img.profilePhoto.toString('base64')
+    try {
+        if (!req.session.user) {
+            return res.redirect('/users/login?msg=Please login first')
+        }
+        const user = await req.session.user
+        const img = await users.findUserById(user._id)
+        let imageBase64=""
+        if(img && img.profilePhoto){
+            imageBase64 = img.profilePhoto.toString('base64')
+        }
+        res.render('editUser', { imageBase64,user })
+    } catch(err) {
+        console.error('Error loading edit user:', err)
+        res.status(500).send('Error loading user data')
     }
-    res.render('editUser', { imageBase64,user })
 })
 
 router.post('/editUser', upload.single('image'), async (req, res) => {
-    const savedUser = await users.updateUser(req.body)
-    req.session.user.name = savedUser
-    res.redirect('/users/editUser')
+    try {
+        if (!req.session.user) {
+            return res.redirect('/users/login?msg=Please login first')
+        }
+        const savedUser = await users.updateUser(req.body)
+        req.session.user.name = savedUser
+        res.redirect('/users/editUser')
+    } catch(err) {
+        console.error('Error updating user:', err)
+        res.status(500).send('Error updating profile')
+    }
 })
 
 router.post('/profile', upload.single('image'), async (req, res) => {
-    const data = fs.readFileSync(path.join(__dirname + '/../uploads/' + req.file.filename))
-    const id = req.session.user._id
-    const a = await users.updateProfilePhoto(data,id)
-    res.redirect('/users/editUser')
+    try {
+        if (!req.session.user) {
+            return res.redirect('/users/login?msg=Please login first')
+        }
+        if (!req.file) {
+            return res.redirect('/users/editUser?msg=No file selected')
+        }
+        const data = fs.readFileSync(path.join(__dirname + '/../uploads/' + req.file.filename))
+        const id = req.session.user._id
+        const a = await users.updateProfilePhoto(data,id)
+        res.redirect('/users/editUser')
+    } catch(err) {
+        console.error('Error updating profile photo:', err)
+        res.status(500).send('Error updating profile photo')
+    }
 })
 
 router.get('/changePassword', async (req, res) => {
-    const id = await req.session.user._id
-    const msg = req.query.msg
-    const passMatch = req.query.passMatch
-    const user = {id,msg,passMatch}
-    res.render('changePassword', {user})
+    try {
+        if (!req.session.user) {
+            return res.redirect('/users/login?msg=Please login first')
+        }
+        const id = await req.session.user._id
+        const msg = req.query.msg
+        const passMatch = req.query.passMatch
+        const user = {id,msg,passMatch}
+        res.render('changePassword', {user})
+    } catch(err) {
+        console.error('Error loading change password:', err)
+        res.status(500).send('Error loading page')
+    }
 })
 
 router.post('/changePassword', async (req, res) => {
-    const user = await users.findUserById(req.session.user._id)
-    const validPassword = await bcrypt.compare(req.body.oldPass, user.password)
-    if (validPassword) {
-        const passMatch = req.body.password === req.body.confirmPass
-        if (passMatch) {
-            const salt = await bcrypt.genSalt(10)
-            const password = await bcrypt.hash(req.body.password, salt)
-            const user = req.body
-            user.password = password
-            users.updatePassword(user).then(values => res.redirect('/users/login?msg=Password Changed Successfully!'))
+    try {
+        if (!req.session.user) {
+            return res.redirect('/users/login?msg=Please login first')
+        }
+        if (!req.body.oldPass || !req.body.password || !req.body.confirmPass) {
+            return res.redirect('/users/changePassword?msg=All fields required')
+        }
+        const user = await users.findUserById(req.session.user._id)
+        const validPassword = await bcrypt.compare(req.body.oldPass, user.password)
+        if (validPassword) {
+            const passMatch = req.body.password === req.body.confirmPass
+            if (passMatch) {
+                if (req.body.password.length < 6) {
+                    return res.redirect('/users/changePassword?msg=Password must be at least 6 characters')
+                }
+                const salt = await bcrypt.genSalt(10)
+                const password = await bcrypt.hash(req.body.password, salt)
+                const updateData = { id: req.session.user._id, password }
+                await users.updatePassword(updateData)
+                res.redirect('/users/login?msg=Password Changed Successfully!')
+            }
+            else {
+                const msg = "Confirm Password Doesn't Match"
+                res.redirect('/users/changePassword?msg='+msg+'&passMatch='+passMatch)
+            }
         }
         else {
-            const msg = "Confirm Password Does\'nt Match"
-            res.redirect('/users/changePassword?msg='+msg+'&passMatch='+passMatch)
+            const msg = "Incorrect Old Password!"
+            res.redirect('/users/changePassword?msg='+msg)
         }
-    }
-    else {
-        const msg = "Incorrect Old Password!"
-        res.redirect('/users/changePassword?msg='+msg)
+    } catch(err) {
+        console.error('Error changing password:', err)
+        res.status(500).send('Error changing password')
     }
 })
 
@@ -178,37 +267,41 @@ router.get('/verifyMail', (req, res) => {
 })
 
 router.post('/verifyMail', async (req, res) => {
-    const mail = users.findUserByMail(req.body).then(values => {
+    try {
+        const values = await users.findUserByMail(req.body)
         if(values){
-        const send = require('gmail-send')({
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD
-        });
-        let code = Math.floor(1000 + Math.random() * 9999)
-        let id = values._id
-        const user = { id, code }
-        users.updateUserCode(user)
-        send({
-            to: req.body.mail,
-            subject: 'Reset Password',
-            html: '<b>To Reset Password, Click the Below Link</b><br>' +
-                'Your password reset secred code is <b>' + code + '</b><br>' +
-                '<a href="https://theblogosphere.herokuapp.com/users/forgotPassword?id=' + values._id +
-                '">click here to Reset Yout Password</a><br> '
-        }, (error, result, fullResult) => {
-            if (error) console.error(error);
-        })
-        if (values.mail) {
-            const msg = "Please Check your E-mail to reset Your Password."
-            res.redirect('/users/login?msg=' + msg)
+            const send = require('gmail-send')({
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD
+            });
+            let code = Math.floor(1000 + Math.random() * 9999)
+            let id = values._id
+            const user = { id, code }
+            await users.updateUserCode(user)
+            const baseUrl = process.env.BASE_URL || `http://${req.get('host')}`
+            const resetLink = `${baseUrl}/users/forgotPassword?id=${values._id}`
+            send({
+                to: req.body.mail,
+                subject: 'Reset Password',
+                html: '<b>To Reset Password, Click the Below Link</b><br>' +
+                    'Your password reset secret code is <b>' + code + '</b><br>' +
+                    `<a href="${resetLink}">click here to Reset Your Password</a><br> `
+            }, (error, result, fullResult) => {
+                if (error) console.error(error);
+            })
+            if (values.mail) {
+                const msg = "Please Check your E-mail to reset Your Password."
+                res.redirect('/users/login?msg=' + msg)
+            }
         }
-    }
         else {
             const msg = "This Email is not Registered!"
             res.redirect('/users/verifyMail?msg='+msg)
         }
-    
-    })
+    } catch(err) {
+        console.error('Error verifying mail:', err)
+        res.status(500).send('Error processing request')
+    }
 })
 
 router.get('/forgotPassword', (req, res) => {
@@ -223,28 +316,41 @@ router.get('/forgotPassword', (req, res) => {
 
 
 router.post('/forgotPassword', async (req, res) => {
-    const id = req.body.id
-    users.findUserById(id).then(async (values) => {
+    try {
+        const id = req.body.id
+        if (!id || !req.body.code || !req.body.password) {
+            return res.redirect('/users/forgotPassword?msg=All fields required&id='+id)
+        }
+        const values = await users.findUserById(id)
+        if (!values) {
+            return res.redirect('/users/forgotPassword?msg=User not found&id='+id)
+        }
         if (req.body.code != "" && values.code !== req.body.code) {
-            res.redirect('/users/forgotPassword?msg=Invalid Code')
+            return res.redirect('/users/forgotPassword?msg=Invalid Code&id='+id)
         }
         const checkPass = (req.body.password === req.body.confirmPass)
         const codeValid = (values.code === req.body.code)
         if ((checkPass) && (codeValid)) {
+            if (req.body.password.length < 6) {
+                return res.redirect('/users/forgotPassword?msg=Password must be at least 6 characters&id='+id)
+            }
             const salt = await bcrypt.genSalt(10)
             const password = await bcrypt.hash(req.body.password, salt)
             const user = { id, password }
             const msg = "Password Changed!!"
-            users.updatePassword(user).then(res.redirect('/users/login?msg=' +msg))
+            await users.updatePassword(user)
+            res.redirect('/users/login?msg=' +msg)
         }
         else {
             const msg = "Password Does not Match"
             const code = req.body.code
             res.redirect('/users/forgotPassword?codeValid='+codeValid+'&msg='
             +msg+'&code='+code+'&id='+id)
-
         }
-    })
+    } catch(err) {
+        console.error('Error resetting password:', err)
+        res.status(500).send('Error resetting password')
+    }
 })
 
 
