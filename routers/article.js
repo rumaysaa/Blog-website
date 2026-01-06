e8ba525af961cec178ad70145632e8d45ee862b1
Rumaysa babulkhair <rumaysa.babulkhair@gmail.com>
2026-01-06 17:26:22 +0000

    bugs

--- routers/article.js
+++ routers/article.js
@@ -3,6 +3,31 @@ const router = express.Router()
 const article = require('../modules/article')
 const catagories = require('../modules/catagories')
 const comments = require('../modules/comments')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'article-' + Date.now() + '-' + file.originalname)
+    }
+});
+
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 },
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = /jpeg|jpg|png|gif/;
+        const ext = allowedTypes.test(file.originalname.toLowerCase());
+        const mime = allowedTypes.test(file.mimetype);
+        if (mime && ext) {
+            return cb(null, true);
+        } else {
+            cb(new Error('Only image files are allowed'));
+        }
+    }
+})
 
 router.get('/' , async(req,res) => {
     try {
@@ -21,29 +46,65 @@ router.get('/json' , async(req,res) => {
 })
 
 router.get('/form' , async(req,res) =>{
-    const cats = await catagories.getCatagories()
-    const id =  await req.query.id
-    const art = await article.getArticleById(id)
-    const data = {cats,art}
-    if(req.session.user){
-    res.render('addArticle' , {data})
-    }
-    else{
-        res.redirect('/users/login?msg=To Write An Article,Please Log In First!!&url=/articles/form')
+    try {
+        const cats = await catagories.getCatagories()
+        const id =  await req.query.id
+        const art = await article.getArticleById(id)
+        const data = {cats,art}
+        if(req.session.user){
+            res.render('addArticle' , {data})
+        }
+        else{
+            res.redirect('/users/login?msg=To Write An Article,Please Log In First!!&url=/articles/form')
+        }
+    } catch(err) {
+        console.error('Error in /form route:', err)
+        res.status(500).send('Error loading form')
     }
 })
 
-router.post('/add', (req,res) => {
-    if(req.body.articleID)
-        article.editArticleById(req.body).then(result => res.redirect('/comments/?id='+req.body.articleID))
-    else
-        article.addArticle(req.body).then(result => res.redirect('/articles'))
+router.post('/add', (req, res, next) => {
+    upload.single('coverPhoto')(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            if (err.code === 'FILE_TOO_LARGE') {
+                return res.redirect('/articles/form?msg=File is too large. Maximum size is 5MB');
+            }
+            return res.redirect('/articles/form?msg=Error uploading file');
+        } else if (err) {
+            return res.redirect('/articles/form?msg=' + err.message);
+        }
+        next();
+    });
+}, async (req, res) => {
+    try {
+        const articleData = req.body;
+        
+        if (req.file) {
+            articleData.coverPhoto = req.file.filename;
+        }
+        
+        if(req.body.articleID) {
+            await article.editArticleById(articleData)
+        } else {
+            await article.addArticle(articleData)
+        }
+        res.redirect('/articles')
+    } catch(err) {
+        console.error('Error adding/editing article:', err)
+        res.status(500).send('Error saving article')
+    }
 })
 
 
 router.get('/delete' , async (req,res) => {
-    const id = await req.query.id
-    article.removeArticleById(id).then(res.redirect('/articles'))
+    try {
+        const id = await req.query.id
+        await article.removeArticleById(id)
+        res.redirect('/articles')
+    } catch(err) {
+        console.error('Error deleting article:', err)
+        res.status(500).send('Error deleting article')
+    }
 })
 
 
--- routers/catagories.js
+++ routers/catagories.js
@@ -2,24 +2,37 @@ const express = require('express')
 const router = express.Router()
 const catagories = require('../modules/catagories')
 
-router.get('/', (req, res) => {
-    const msg = req.query.Error
-    catagories.getCatagories().then(cat => {
+router.get('/', async (req, res) => {
+    try {
+        const msg = req.query.Error
+        const cat = await catagories.getCatagories()
         const data = {msg,cat}
         res.render('catagories', {data})
-    })
+    } catch(err) {
+        console.error('Error loading categories:', err)
+        res.status(500).send('Error loading categories')
+    }
 })
 
 router.post('/' , async (req,res) => {
-    const catagory = await req.body.catName
-    const getCat = await catagories.searchCat(catagory)
-    //console.log(getCat)
-    if(getCat !== null){
-        const msg = "Catagory already exist!"
-        res.redirect('/catagories?Error='+ msg)
+    try {
+        if (!req.body.catName) {
+            return res.redirect('/catagories?Error=Category name required')
+        }
+        const catagory = await req.body.catName
+        const getCat = await catagories.searchCat(catagory)
+        if(getCat !== null){
+            const msg = "Category already exists!"
+            res.redirect('/catagories?Error='+ msg)
+        }
+        else {
+            await catagories.addCatagory(req.body)
+            res.redirect('/catagories')
+        }
+    } catch(err) {
+        console.error('Error adding category:', err)
+        res.status(500).send('Error adding category')
     }
-    else
-        catagories.addCatagory(req.body).then(result => res.redirect('/catagories'))
 })
 
 
